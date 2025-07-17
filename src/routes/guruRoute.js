c5dcddd7819978dbeb8cd712f2035ac6c6e84eff
Muhammad Yassir Dzaky Ilyas <161009893+yassirilyas@users.noreply.github.com>
2025-07-17 12:40:29 +0700

    Update guruRoute.js

--- src/routes/guruRoute.js
+++ src/routes/guruRoute.js
@@ -5,7 +5,7 @@ const formParser = require('../middleware/formParser');
 
 router.get('/', guruController.getAllGuru);
 router.get('/nomor-induk/:nomor_induk', guruController.getGuruByNomorInduk);
-router.get('/id-guru/:id_guru', guruController.getGuruById);
+router.get('/:id_guru', guruController.getGuruById);
 router.post('/', formParser, guruController.createGuru);
 router.put('/:id_guru', guruController.updateGuru);
 router.delete('/:id_guru', guruController.deleteGuru);
