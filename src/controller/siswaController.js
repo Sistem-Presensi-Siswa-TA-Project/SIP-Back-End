0c2a700a28d924b4c52cd367522b8c87117e652e
Muhammad Yassir Dzaky Ilyas <161009893+yassirilyas@users.noreply.github.com>
2025-07-18 00:13:10 +0700

    Update siswaController.js

--- src/controller/siswaController.js
+++ src/controller/siswaController.js
@@ -20,7 +20,7 @@ exports.getAllSiswa = async (req, res) => {
 
 // GET siswa berdasarkan kelas
 exports.getSiswaByKelas = async (req, res) => {
-    const { kelas } = req.body;
+    const { kelas } = req.params;
 
     if (!kelas) {
         return res.status(400).json({
