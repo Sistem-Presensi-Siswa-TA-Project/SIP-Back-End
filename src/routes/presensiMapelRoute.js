22cbf4bd250e10173ea8ea0e6993f8a60d71471a
Azra Nadhira Aulia <157966620+azranadhira@users.noreply.github.com>
2025-07-19 22:38:48 +0700

    Update presensiMapelRoute.js

--- src/routes/presensiMapelRoute.js
+++ src/routes/presensiMapelRoute.js
@@ -7,6 +7,7 @@ const formParser = require('../middleware/formParser');
 router.get('/', presensiMapelController.getAllPresensiMapel);
 router.get('/:id', presensiMapelController.getPresensiMapelById);
 router.get('/:kelas', presensiMapelController.getPresensiMapelByKelas);
+router.post('/search-form', presensiMapelController.searchPresensiMapelByForm);
 router.post('/', formParser, presensiMapelController.createPresensiMapel);
 router.put('/:id', formParser, presensiMapelController.updatePresensiMapel);
 router.delete('/:id', presensiMapelController.deletePresensiMapel);
