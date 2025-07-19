2bc56bec7a49d2f53bdc5c80d8ebed061f053ae4
Azra Nadhira Aulia <157966620+azranadhira@users.noreply.github.com>
2025-07-19 21:25:51 +0700

    Add files via upload

---
+++ src/routes/presensiMapelRoute.js
@@ -0,0 +1,14 @@
+// routes/presensiMapelRoutes.js
+const express = require('express');
+const router = express.Router();
+const presensiMapelController = require('../controller/presensiMapelController');
+const formParser = require('../middleware/formParser');
+
+router.get('/', presensiMapelController.getAllPresensiMapel);
+router.get('/:id', presensiMapelController.getPresensiMapelById);
+router.get('/:kelas', presensiMapelController.getPresensiMapelByKelas);
+router.post('/', formParser, presensiMapelController.createPresensiMapel);
+router.put('/:id', formParser, presensiMapelController.updatePresensiMapel);
+router.delete('/:id', presensiMapelController.deletePresensiMapel);
+
+module.exports = router;
---
+++ src/routes/presensiPiketRoute.js
@@ -0,0 +1,14 @@
+// routes/presensiPiketRoutes.js
+const express = require('express');
+const router = express.Router();
+const presensiPiketController = require('../controller/presensiPiketController');
+const formParser = require('../middleware/formParser');
+
+router.get('/', presensiPiketController.getAllPresensiPiket);
+router.get('/:id', presensiPiketController.getPresensiPiketById);
+router.get('/kelas/:kelas', presensiPiketController.getPresensiPiketByKelas);
+router.post('/', formParser, presensiPiketController.createPresensiPiket);
+router.put('/:id', formParser, presensiPiketController.updatePresensiPiket);
+router.delete('/:id', presensiPiketController.deletePresensiPiket);
+
+module.exports = router;
