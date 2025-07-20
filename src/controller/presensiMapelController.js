3fec3216cbf4b79c3b1ca97c044943bd93a657f3
Muhammad Yassir Dzaky Ilyas <161009893+yassirilyas@users.noreply.github.com>
2025-07-20 23:57:04 +0700

    Update presensiMapelController.js

--- src/controller/presensiMapelController.js
+++ src/controller/presensiMapelController.js
@@ -59,6 +59,19 @@ exports.getPresensiMapelById = async (req, res) => {
   }
 };
 
+// READ BY ID JADWAL
+exports.getPresensiMapelById = async (req, res) => {
+  const { idJadwal } = req.params;
+  try {
+    const [rows] = await pool.execute('SELECT * FROM Presensi_Mapel WHERE id_jadwal = ?', [idJadwal]);
+    if (rows.length === 0) return res.status(404).json({ message: 'Data tidak ditemukan' });
+    res.json({ message: 'Data ditemukan', data: rows[0] });
+  } catch (err) {
+    res.status(500).json({ message: 'Gagal ambil data', error: err.message });
+  }
+};
+
+// READ BY KELAS
 exports.getPresensiMapelByKelas = async (req, res) => {
   const { kelas } = req.params;
 
