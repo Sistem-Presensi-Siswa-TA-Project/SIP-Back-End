97134de59c5b24c04a8059631f963c56f78c3d7a
Muhammad Yassir Dzaky Ilyas <161009893+yassirilyas@users.noreply.github.com>
2025-07-21 04:06:20 +0700

    Update presensiMapelRoute.js

--- src/routes/presensiMapelRoute.js
+++ src/routes/presensiMapelRoute.js
@@ -11,7 +11,7 @@ router.get('/jadwal/:idJadwal/tanggal/:tanggal', presensiMapelController.getPres
 router.get('/kelas/:kelas', presensiMapelController.getPresensiMapelByKelas);
 router.post('/search', presensiMapelController.searchPresensiMapelByForm);
 router.post('/', formParser, presensiMapelController.createPresensiMapel);
-router.put('/', formParser, presensiMapelController.updatePresensiMapel);
+router.put('/update', formParser, presensiMapelController.updatePresensiMapel);
 router.delete('/:id', presensiMapelController.deletePresensiMapel);
 
 module.exports = router;
