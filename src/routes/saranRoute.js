646b224c3635ba6a3ec342adc4424a56d5581309
Muhammad Yassir Dzaky Ilyas <161009893+yassirilyas@users.noreply.github.com>
2025-07-21 11:16:59 +0700

    Update saranRoute.js

--- src/routes/saranRoute.js
+++ src/routes/saranRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const saranController = require('../controllers/saranController');
 
 router.post('/', saranController.createSaran);
+router.get('/', saranController.getAllSaran);
 
 module.exports = router;
