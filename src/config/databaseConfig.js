8c20c7a9163967b1b8d8f38a73e867cee0cd27f3
Muhammad Yassir Dzaky Ilyas <161009893+yassirilyas@users.noreply.github.com>
2025-07-22 16:27:07 +0700

    Update databaseConfig.js

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -9,7 +9,8 @@ const db = mysql.createPool({
   database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  dateStrings: true,
 });
 
 module.exports = db;
