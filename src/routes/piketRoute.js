8ea4505989e53c4c93b4e299a999c665f5e557c6
Muhammad Yassir Dzaky Ilyas <161009893+yassirilyas@users.noreply.github.com>
2025-07-22 18:38:40 +0700

    Update piketRoute.js

--- src/routes/piketRoute.js
+++ src/routes/piketRoute.js
@@ -8,6 +8,7 @@ router.get('/:id_piket', piketController.getPiketById);
 router.get('/kode/:kodePiket', piketController.getPiketByKodePiket);
 router.post('/', formParser, piketController.createPiket);
 router.put('/:id_piket', formParser, piketController.updatePiket);
+router.delete('/', piketController.deleteAllPiket);
 router.delete('/:id_piket', piketController.deletePiket);
 
 module.exports = router;
